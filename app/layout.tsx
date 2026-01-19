05784cc4edfc81c0373dad605c5aa249da4c7973
dylan <dylanhacquard@gmail.com>
2026-01-19 11:05:58 +0100

    speed insight add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Dylan Hacquard | Administrateur Réseaux & Système",
@@ -47,7 +48,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className="dark">
-      <body className="min-h-screen">{children}</body>
+      <body className="min-h-screen">{children}
+        <SpeedInsights />
+        <Analytics /> 
+      </body>
     </html>
   );
 }
